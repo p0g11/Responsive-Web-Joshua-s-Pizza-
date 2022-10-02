89c1560950b6a0120d40e3555b1276bbb9ed71a4
p0g11 <qjjbsazon@tip.edu.ph>
2022-10-02 12:35:23 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function sample1 (){
+    alert ("Welcome to Joshua's Pizza");
+    }
+  
+    
+    function submit(){
+        alert ("Thank you for filling up. We will update your order as soon as possible.");
+    }
+  submit
